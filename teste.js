aae902437995fdf263f9788360e883eadcd3304a
thiagomag94 <thiagomag94@gmail.com>
2022-10-14 16:20:24 -0300

    first commit

---
+++ teste.js
@@ -0,0 +1,51 @@
+
+
+var div = document.getElementById('div');
+div.innerHTML = '<ul id="lista" class="flex  justify-center  items-center text-stone-900 w-full h-full gap gap-10 flex-wrap"></ul>';
+var ul = document.getElementById('lista');
+var label = document.getElementById('label');
+
+async function sorteia() {
+    ul.innerHTML = ''
+    var contador = 0
+    var lista_dezenas = Array()
+
+    while (contador<6) {
+
+        var numero = Math.floor(Math.random(60)*60+1);
+        if (lista_dezenas.includes(numero) == false){
+            lista_dezenas.push(numero);
+            contador = contador + 1;
+    
+        }              
+    }
+    for(var i=0; i < lista_dezenas.length; i++){
+        
+      
+        appendNumber(i, lista_dezenas)
+        await time(0.7).then(function(data){
+            console.log(data);
+        })
+    }
+        
+}    
+
+function appendNumber(index, lista){
+
+    
+    console.log('inserido a dezena '+(index+1))
+    var li = document.createElement('li');
+    li.classList.add('rounded-full', 'bg-yellow-500', 'py-3', 'px-4')
+    li.innerText = `${lista[index]}`;
+    ul.append(li)
+}
+
+
+
+
+function time(seconds){
+    let mili = seconds*1000;
+    return new Promise(res => 
+        setTimeout(res(`promise resolvida após ${mili} ms `), mili)
+    )
+}
